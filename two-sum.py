90868046d04b0e820e18c9a30695633d439c9be8
Dmitriy <udv8@ya.ru>
2023-03-30 12:23:56 +0300

    fix two-sum

--- two-sum.py
+++ two-sum.py
@@ -1,13 +1,19 @@
 class Solution:
     def twoSum(self, nums, target):
-        res_list = []
-        for i in range(len(nums)-1):
-            res_list = [i]
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    res_list.append(j)
-                    return res_list
-        return -1
+        # res_list = []
+        # for i in range(len(nums)-1):
+        #     res_list = [i]
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             res_list.append(j)
+        #             return res_list
+        # return -1
+        hash = {}
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in hash:
+                return [i, hash[i]]
+            hash[nums[i]] = i
 
 
 exemple = Solution()
